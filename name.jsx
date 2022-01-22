f3cd04ccee77614b656210fd4ff601de8100131f
emme-ario <emme-ario@users.noreply.github.com>
2022-01-22 08:35:46 +0100

    Remake with searchbar

---
+++ name.jsx
@@ -0,0 +1,19 @@
+class Main {
+
+    constructor(temperature) {
+        this.Main = Main
+        this.temperature = temperature
+    }
+}
+
+function JSONToMain(jsonObject) {
+    const { 
+        Main: Main,
+        temperature: temperature,
+    } = jsonObject // destructuring
+
+    const Main = new Main(Main, temperature)
+
+    console.log(Main)
+    return Main
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,65 @@
 import React,{useState,useEffect} from 'react';
-// import './App.css'
 
-const API_KEY =  "0df3125086d55babcbef0eb570ec442b"
+
+const API_KEY =  "d5eadf8e319f2c179bf8c1501a4338f5"
 
 function App() {
+  const [searchVisible,setSearchVisible] = useState(true)
   const [weather,setWeather] = useState({})
+  const [city, setCity] = useState("")
 
-  useEffect(()=>{
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Brescia&appid=${API_KEY}`)
+  function setVisible(domId, visible) {
+    let value = "none"
+    if(visible === true) {
+      value = "block"
+    }
+    document.getElementById(domId).style.display= value
+  }
+
+  function setContent(domId,content) {
+    document.getElementById(domId).innerHTML = content 
+  }
+  
+  function CercaDati(ev) {
+    ev.preventDefault();
+    setSearchVisible(false)
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
     .then(res => res.json())
     .then(body => setWeather(body))
-  },[])
+    
+    /*
+    setContent("weather.icon",weather.weather[0].icon)
+    setContent("temperature",weather.main.temp)
+    setContent("weather.description",weather.weather[0].description)
+    setContent("weather.main",weather.weather[0].main)
+    */
 
+    return false   
+    
+  }
+
+  useEffect(() => {
+    if (!weather) return
+    //setVisible("recipe", true)
+  }, [weather])
+    
   return (
-    <main>
+    <div className="mainDiv">
+      {searchVisible &&
+        <form onSubmit={(ev) => CercaDati(ev)}>
+          <input type="text" value={city} onChange={(ev)=> setCity(ev.target.value)}/> 
+          <input type="submit" value="cerca"></input>
+        </form>
+      }
+
       <pre>{JSON.stringify(weather,null,2)}</pre>
-    </main>
+      
+      {(typeof weather.main != "undefined") ? (
+        <div className="location">{weather.main.temp},{weather.weather[0].id}</div>
+        ) : ('')}
+    </div>
   );
 }
 
-export default App
+export default App;
