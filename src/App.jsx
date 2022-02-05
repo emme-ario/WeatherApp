28f1c7cf5d48811ebb3317779a85856aa678849c
Matteo Mariottini <matteomariottini9@gmail.com>
2022-02-05 09:38:02 +0100

    Searchbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,59 +4,37 @@ import PrintData from './WeatherPage';
 const API_KEY =  "d5eadf8e319f2c179bf8c1501a4338f5"
 
 function App() {
-  const [searchVisible,setSearchVisible] = useState(true)
   const [weather,setWeather] = useState({})
   const [city, setCity] = useState("")
 
-  function setVisible(domId, visible) {
-    let value = "none"
-    if(visible === true) {
-      value = "block"
-    }
-    document.getElementById(domId).style.display= value
-  }
 
-  function setContent(domId,content) {
-    document.getElementById(domId).innerHTML = content 
-  }
-  
   function CercaDati(ev) {
-    ev.preventDefault();
-    setSearchVisible(false)
+    ev.preventDefault()
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
     .then(res => res.json())
     .then(body => setWeather(body))
-    
-    /*
-    setContent("weather.icon",weather.weather[0].icon)
-    setContent("temperature",weather.main.temp)
-    setContent("weather.description",weather.weather[0].description)
-    setContent("weather.main",weather.weather[0].main)
-    */
-
     return false   
     
   }
 
   useEffect(() => {
     if (!weather) return
-    //setVisible("recipe", true)
   }, [weather])
     
   return (
     <div className="mainDiv">
-      {searchVisible &&
-        <div className="uk-card uk-card-primary uk-card-hover uk-card-body uk-light">
-            <form onSubmit={(ev) => CercaDati(ev)} className="uk-align-center uk-search uk-search-navbar">
-              {/* <span uk-search-icon></span> */}
-              <input value={city} onChange={(ev)=> setCity(ev.target.value)} className="uk-search-input" type="search" placeholder="Search"></input>
+      <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-center">
+          <div className="uk-navbar-item">
+            <form onSubmit={(ev) => CercaDati(ev)} className="uk-search uk-search-navbar">
+              <span uk-search-icon></span>
+              <input value={city} onChange={(ev)=> setCity(ev.target.value)} className="uk-search-input" type="search" placeholder="Search location..."></input>
             </form>
+          </div>
         </div>
-      }
+      </nav>
 
-      {/* <pre>{JSON.stringify(weather,null,2)}</pre> */}
-      
       {(typeof weather.main != "undefined") ? (
         <PrintData weather={weather}/>
         ) : ('')}
--- src/WeatherPage.jsx
+++ src/WeatherPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function PrintData({weather}) {
     return <div className="uk-text-center">
-        {/* code here */}
         <h1 className="uk-card uk-card-primary uk-card-hover uk-card-body uk-light"> <img  width="75" height="75" src={"https://openweathermap.org/img/wn/"+ weather.weather[0].icon + ".png"}/> {weather.name} <img  width="75" height="75" src={"https://openweathermap.org/img/wn/"+ weather.weather[0].icon + ".png"}/> </h1>
         <h3 className="uk-card uk-card-hover uk-card-small uk-card-default uk-card-body"> {(weather.main.temp - 273.15).toFixed(1) + '°C'}</h3>
         <div className="uk-flex-inline uk-flex-center uk-flex-middle">
